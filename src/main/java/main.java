9d20252fdecec53ff572fa2f08e5ab0f8784b133
secheverria <sebastian.echeverria@gmail.com>
2017-05-02 16:50:08 -0300

    Ace-RS: initial repo cretion.

---
+++ src/main/java/main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Sebastian on 2017-05-02.
+ */
+public class main {
+
+    public static void main(String[] args)
+    {
+        try {
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+    }
+
+}
