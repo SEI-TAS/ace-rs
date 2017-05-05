ea8a339a30b859d8ac2204138c91ec767a0238bc
secheverria <sebastian.echeverria@gmail.com>
2017-05-05 17:36:58 -0300

    RS: created initial test CoapsRS, runs but not really tested does works properly.

---
+++ src/main/java/CoapsRS.java
@@ -0,0 +1,110 @@
+/**
+ * Created by Sebastian on 2017-05-05.
+ */
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.*;
+import java.util.logging.Logger;
+
+import COSE.*;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.config.NetworkConfig;
+import org.eclipse.californium.scandium.DTLSConnector;
+import org.eclipse.californium.scandium.config.DtlsConnectorConfig;
+import org.eclipse.californium.scandium.dtls.cipher.CipherSuite;
+
+import se.sics.ace.AceException;
+import se.sics.ace.COSEparams;
+import se.sics.ace.TimeProvider;
+import se.sics.ace.as.PDP;
+import se.sics.ace.coap.as.CoapAceEndpoint;
+import se.sics.ace.coap.rs.DTLSProfilePskStore;
+import se.sics.ace.cwt.CwtCryptoCtx;
+import se.sics.ace.examples.KissTime;
+import se.sics.ace.examples.KissValidator;
+import se.sics.ace.rs.AudienceValidator;
+import se.sics.ace.rs.AuthzInfo;
+import se.sics.ace.rs.ScopeValidator;
+import se.sics.ace.rs.TokenRepository;
+
+/**
+ * A resource server listening to CoAP requests over DTLS.
+ *
+ * Create an instance of this server with the constructor then call
+ * CoapsAS.start();
+ *
+ * @author Sebastian Echeverria
+ *
+ */
+public class CoapsRS extends CoapServer implements AutoCloseable {
+
+    /**
+     * The logger
+     */
+    private static final Logger LOGGER = Logger.getLogger(CoapsRS.class.getName());
+
+    private static byte[] key128 = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+    /**
+     * The token endpoint
+     */
+    private AuthzInfo authInfoResource = null;
+
+    /**
+     * The introspect endpoint
+     */
+    private CoapAceEndpoint authInfoEndpoint;
+
+    /**
+     * Constructor.
+     *
+     * @param time
+     * @param asymmetricKey
+     * @throws AceException
+     * @throws CoseException
+     *
+     */
+    public CoapsRS(TimeProvider time, Set<String> resources,
+                   AudienceValidator audValidator, ScopeValidator scopeValidator,
+                   OneKey asymmetricKey) throws AceException, CoseException, IOException {
+
+        COSEparams coseP = new COSEparams(MessageTag.Encrypt0, AlgorithmID.AES_CCM_16_128_128, AlgorithmID.Direct);
+        CwtCryptoCtx ctx = CwtCryptoCtx.encrypt0(key128, coseP.getAlg().AsCBOR());
+
+        TokenRepository tokenRepo = new TokenRepository(scopeValidator, resources,"src/test/resources/tokens.json", ctx);
+        List<String> issuers = Collections.singletonList("TestAS");
+        this.authInfoResource = new AuthzInfo(tokenRepo, issuers, time, null, audValidator, ctx);
+        this.authInfoEndpoint = new CoapAceEndpoint("auth-info", this.authInfoResource);
+        add(this.authInfoEndpoint);
+
+        DtlsConnectorConfig.Builder config = new DtlsConnectorConfig.Builder(
+                new InetSocketAddress(CoAP.DEFAULT_COAP_SECURE_PORT));
+        if (asymmetricKey != null && asymmetricKey.get(KeyKeys.KeyType) == KeyKeys.KeyType_EC2 ) {
+            LOGGER.info("Starting CoapsRS with PSK and RPK");
+            config.setSupportedCipherSuites(new CipherSuite[]{
+                    CipherSuite.TLS_PSK_WITH_AES_128_CCM_8,
+                    CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8});
+        } else {
+            LOGGER.info("Starting CoapsRS with PSK only");
+            config.setSupportedCipherSuites(new CipherSuite[]{
+                    CipherSuite.TLS_PSK_WITH_AES_128_CCM_8});
+        }
+
+        DTLSProfilePskStore store = new DTLSProfilePskStore(this.authInfoResource);
+        config.setPskStore(store);
+        if (asymmetricKey != null) {
+            config.setIdentity(asymmetricKey.AsPrivateKey(), asymmetricKey.AsPublicKey());
+        }
+
+        DTLSConnector connector = new DTLSConnector(config.build());
+        addEndpoint(new CoapEndpoint(connector, NetworkConfig.getStandard()));
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOGGER.info("Closing down CoapsRS ...");
+        this.authInfoEndpoint.close();
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,48 @@
+import COSE.KeyKeys;
+import COSE.OneKey;
+import com.upokecenter.cbor.CBORObject;
+import se.sics.ace.examples.KissTime;
+import se.sics.ace.examples.KissValidator;
+
+import java.util.*;
+
 /**
  * Created by Sebastian on 2017-05-02.
  */
 public class main {
+    static byte[] key256 = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,28, 29, 30, 31, 32};
 
     public static void main(String[] args)
     {
         try {
+
+            Set<String> actions = new HashSet<>();
+            actions.add("GET");
+            Map<String, Set<String>> myResource = new HashMap<>();
+            myResource.put("temp", actions);
+            Map<String, Map<String, Set<String>>> myScopes = new HashMap<>();
+            myScopes.put("r_temp", myResource);
+
+            myResource.clear();
+            myResource.put("co2", actions);
+            myScopes.put("r_co2", myResource);
+
+            KissValidator valid = new KissValidator(Collections.singleton("rs1"), myScopes);
+
+            Set<String> resources = new HashSet<>();
+            resources.add("temp");
+            resources.add("co2");
+
+            CBORObject keyData = CBORObject.NewMap();
+            keyData.Add(KeyKeys.KeyType.AsCBOR(), KeyKeys.KeyType_Octet);
+            keyData.Add(KeyKeys.Octet_K.AsCBOR(), CBORObject.FromObject(key256));
+            OneKey sharedKey = new OneKey(keyData);
+
+            CoapsRS rsServer = new CoapsRS(new KissTime(), resources, valid, valid, null);
+
+            System.out.println("Starting server");
+            rsServer.start();
+
         } catch (Exception e)
         {
             e.printStackTrace();
